f8af47b9cd90bb76e38aeb658f17d4980dd53158
TasneemAhmed <49993791+TasneemAhmed@users.noreply.github.com>
2024-03-20 10:04:30 +0000

    Add load_jobs_data pipeline which load data to GCS bucket

---
+++ mage-load-data/data_loaders/read_jobs_data.py
@@ -0,0 +1,21 @@
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+import pandas as pd
+
+@data_loader
+def load_data(*args, **kwargs):
+   
+   csv_file = "/home/src/dataset/jobs_in_data_2024.csv"
+
+   return pd.read_csv(csv_file)
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
